e7c148f8af984600bec90fae2df731f6b4ab6864
akhmadhurshetov <122173327+akhmadhurshetov@users.noreply.github.com>
2023-11-17 00:01:26 +0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,16 +6,20 @@
 
 #include <iostream>
 #include "task_1.h"
+#include "task_2.h"
+#include "task_3.h"
+#include "task_4.h"
+
 
 int main() {
     std::cout << "Task 1" << std::endl;
-    // call for task 1
+    bubbleSort();
     std::cout << "Task 2" << std::endl;
-    // call for task 2
+    selectionSort();
     std::cout << "Task 3" << std::endl;
-    // call for task 3
+    rearrangeSoldiers();
     std::cout << "Task 4" << std::endl;
-    // call for task 4
+    countElements();
     std::cout << "Task 5" << std::endl;
     // call for task 5
     return 0;
